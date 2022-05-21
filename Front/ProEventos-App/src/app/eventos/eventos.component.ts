3b79d096a47c789ce5f170873f156db7f13e36d0
Gustavo <gustavohs2004@gmail.com>
2022-05-21 10:54:43 -0300

    Criando a requisicao HTTP no projeto angular

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,22 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent implements OnInit {
-  public eventos: any = [
-    {
-      Tema: 'Angular 11',
-      Local: 'Belo Horizonte',
-    },
-    {
-      Tema: '.NET 5',
-      Local: 'São Paulo',
-    },
-    {
-      Tema: 'Angular e suas novidades',
-      Local: 'Rio de Janeiro',
-    }
-  ];
+  public eventos: any;
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getEventos();
+  }
+
+  public getEventos(): void {
+    this.http.get('https://localhost:5100/api/eventos').subscribe(
+      response => this.eventos = response,
+      error => console.log(error),
+    );
+  }
 }
