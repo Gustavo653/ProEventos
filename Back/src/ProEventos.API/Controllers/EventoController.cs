1c5eb96ad6a710bb2ba58a087a5ee7f3ba841910
Gustavo <gustavohs2004@gmail.com>
2022-05-21 09:45:55 -0300

    Adicionando o contexto ao BD, recriando verbos get

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -12,44 +13,24 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-        public EventoController()
+        private readonly DataContext _context;
+        public EventoController(DataContext context)
         {
-
+            this._context = context;
         }
-
-        public readonly IEnumerable<Evento> _evento = new Evento[]{
-            new Evento()
-            {
-                EventoId = 1,
-                Tema = "Angular 11 e .NET 5",
-                Local = "Belo Horizonte",
-                Lote = "1 lote",
-                QtdPessoas = 250,
-                DataEvento = DateTime.Now.AddDays(2).ToString()
-            },
-            new Evento()
-            {
-                EventoId = 2,
-                Tema = "Angular 22 e .NET 2",
-                Local = "Blumenau",
-                Lote = "3 lote",
-                QtdPessoas = 1000,
-                DataEvento = DateTime.Now.AddDays(50).ToString()
-            }
-        };
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return _context.Eventos;
         }
         [HttpGet("{id}")]
         public IEnumerable<Evento> GetById(int id)
         {
-            return _evento.Where(evento => evento.EventoId == id);
+            return _context.Eventos.Where(evento => evento.EventoId == id);
         }
         [HttpPost]
-        public IEnumerable<Evento> Post()
+        public IEnumerable<Evento> Post(Evento evento)
         {
             return _evento;
         }
