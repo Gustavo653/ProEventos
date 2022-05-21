3a3f695e45916f53fb7da62feb4a42458eb77944
Gustavo <gustavohs2004@gmail.com>
2022-05-21 09:09:50 -0300

    Adicionando classe evento, modificando EventoController

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
 {
@@ -16,10 +17,51 @@ namespace ProEventos.API.Controllers
 
         }
 
+        public readonly IEnumerable<Evento> _evento = new Evento[]{
+            new Evento()
+            {
+                EventoId = 1,
+                Tema = "Angular 11 e .NET 5",
+                Local = "Belo Horizonte",
+                Lote = "1 lote",
+                QtdPessoas = 250,
+                DataEvento = DateTime.Now.AddDays(2).ToString()
+            },
+            new Evento()
+            {
+                EventoId = 2,
+                Tema = "Angular 22 e .NET 2",
+                Local = "Blumenau",
+                Lote = "3 lote",
+                QtdPessoas = 1000,
+                DataEvento = DateTime.Now.AddDays(50).ToString()
+            }
+        };
+
         [HttpGet]
-        public string Get()
+        public IEnumerable<Evento> Get()
         {
-            return "value";
+            return _evento;
+        }
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+            return _evento.Where(evento => evento.EventoId == id);
+        }
+        [HttpPost]
+        public IEnumerable<Evento> Post()
+        {
+            return _evento;
+        }
+        [HttpPut("{id}")]
+        public IEnumerable<Evento> Put(int id)
+        {
+            return _evento;
+        }
+        [HttpDelete("{id}")]
+        public IEnumerable<Evento> Delete(int id)
+        {
+            return _evento;
         }
     }
 }
