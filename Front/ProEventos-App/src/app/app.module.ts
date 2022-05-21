a5cd2f0c2be5040c99337f75bf820ed5ea89bfc4
Gustavo <gustavohs2004@gmail.com>
2022-05-21 13:49:36 -0300

    Adicionando navbar nas páginas

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 @NgModule({
   declarations: [
@@ -14,8 +16,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     EventosComponent,
     EventosComponent,
     PalestrantesComponent,
+    NavComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    CollapseModule.forRoot(),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
