9da4d8136cadc512ce955372cea4fd22bf9a7399
Gustavo <gustavohs2004@gmail.com>
2022-05-28 22:52:03 -0300

    Tornando os forms reativos

--- Front/ProEventos-App/src/app/components/user/registration/registration.component.ts
+++ Front/ProEventos-App/src/app/components/user/registration/registration.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.scss']
+  styleUrls: ['./registration.component.scss'],
 })
 export class RegistrationComponent implements OnInit {
-
-  constructor() { }
+  form!: FormGroup;
+  get f(): any {
+    return this.form.controls;
+  }
+  constructor(public fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.validation();
   }
-
+  private validation(): void {
+    this.form = this.fb.group({
+      primeiroNome: ['', [Validators.required]],
+      ultimoNome: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      userName: ['', [Validators.required]],
+      senha: ['', [Validators.required, Validators.minLength(6)]],
+      confirmeSenha: ['', [Validators.required]],
+    });
+  }
 }
